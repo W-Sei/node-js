c238a34a932e89c41918ed663459a0c0e9114e74
Hanna Franko <sei.wrcl@gmail.com>
2023-05-14 17:37:30 +0300

    add task

--- contacts.js
+++ contacts.js
@@ -1,1 +1,23 @@
-console.log("test");
+// console.log("test");
+
+/*
+ * Розкоментуйте і запиши значення
+ * const contactsPath = ;
+ */
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
